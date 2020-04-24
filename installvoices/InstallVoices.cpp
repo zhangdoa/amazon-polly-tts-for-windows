e77cb1796f46ea590a98c653f3ff2325c7239172
Larson <troylar+github@pm.me>
2020-04-24 12:03:10 -0400

    Add Lupe newscaster

--- installvoices/InstallVoices.cpp
+++ installvoices/InstallVoices.cpp
@@ -137,8 +137,10 @@ voice_map_t SelectedVoicesMap(const std::wstring& voiceList)
 					pollyVoices.insert(std::make_pair(name, v4sp_neural));
 				}
 				name = converter.from_bytes(VoiceIdMapper::GetNameForVoiceId(voice.GetId()).c_str());
-				if (name == L"Matthew" || name == L"Joanna")
+				std::wcout << name << L"\n";
+				if (name == L"Matthew" || name == L"Joanna" || name == L"Lupe")
 				{
+					std::wcout << "Adding newscaster and conversational\n";
 					VoiceForSAPI v4sp_newscaster(voice, true, true, false);
 					name.append(L"_newscaster");
 					pollyVoices.insert(std::make_pair(name, v4sp_newscaster));
