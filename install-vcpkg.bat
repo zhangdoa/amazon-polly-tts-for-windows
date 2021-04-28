e26e3b8fc7544cb725a0e7ebc5c9dea8c4d91f40
Troy Larson <troylar+myatlassian@pm.me>
2021-04-28 17:25:16 -0400

    Automate vcpkg installation

--- install-vcpkg.bat
+++ install-vcpkg.bat
@@ -1,3 +1,12 @@
+@echo off
+echo Cloning vcpkg
+git clone https://github.com/microsoft/vcpkg ..\vcpkg --depth=1
+echo Bootstrapping vcpkg
+pushd .
+cd ..\vcpkg
 call .\bootstrap-vcpkg.bat
+echo Integrating vcpkg with Visual Studio
 vcpkg integrate install
+echo Installing packages
 vcpkg install sdl2:x64-windows sdl2:x86-windows tinyxml2:x64-windows tinyxml2:x86-windows  boost-algorithm[core]:x64-windows  boost-algorithm[core]:x86-windows spdlog:x86-windows spdlog:x64-windows aws-sdk-cpp[text-to-speech]:x86-windows aws-sdk-cpp[text-to-speech]:x64-windows rapidjson:x64-windows rapidjson:x86-windows --recurse
+popd
