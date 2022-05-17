1f3b069a8083127645e650c35e04c85947eecb22
zhangdoa <zhangandhang@gmail.com>
2022-05-18 00:12:36 +0200

    Add missing vcpkg dependency.

--- install-vcpkg.bat
+++ install-vcpkg.bat
@@ -8,5 +8,5 @@ call .\bootstrap-vcpkg.bat
 echo Integrating vcpkg with Visual Studio
 vcpkg integrate install
 echo Installing packages
-vcpkg install sdl2:x64-windows sdl2:x86-windows tinyxml2:x64-windows tinyxml2:x86-windows  boost-algorithm[core]:x64-windows  boost-algorithm[core]:x86-windows spdlog:x86-windows spdlog:x64-windows aws-sdk-cpp[text-to-speech]:x86-windows aws-sdk-cpp[text-to-speech]:x64-windows rapidjson:x64-windows rapidjson:x86-windows --recurse
+vcpkg install sdl2:x64-windows sdl2:x86-windows tinyxml2:x64-windows tinyxml2:x86-windows  boost-algorithm[core]:x64-windows  boost-algorithm[core]:x86-windows spdlog:x86-windows spdlog:x64-windows aws-sdk-cpp[text-to-speech]:x86-windows aws-sdk-cpp[text-to-speech]:x64-windows aws-sdk-cpp[polly]:x86-windows aws-sdk-cpp[polly]:x64-windows rapidjson:x64-windows rapidjson:x86-windows --recurse
 popd
